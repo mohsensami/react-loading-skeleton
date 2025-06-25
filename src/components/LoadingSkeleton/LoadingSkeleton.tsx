9ba59aa35b8f5eb1d74edeb2b67e0ca96add0ca3
mohsensami <mohsensami18727@gmail.com>
2025-06-25 19:36:19 +0330

    show default skeleton components

--- src/components/LoadingSkeleton/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -1,9 +1,49 @@
 import styles from "./LoadingSkeleton.module.css";
+import React from "react";
 
-export const LoadingSkeleton = () => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+interface LoadingSkeletonProps {
+  width?: number | string;
+  height?: number | string;
+  variant?: "text" | "circle" | "rect";
+}
+
+const getBorderRadius = (variant: string | undefined) => {
+  switch (variant) {
+    case "circle":
+      return "50%";
+    case "rect":
+      return "4px";
+    case "text":
+    default:
+      return "4px";
+  }
 };
+
+const getDefaultSize = (variant: string | undefined) => {
+  switch (variant) {
+    case "circle":
+      return { width: 40, height: 40 };
+    case "rect":
+      return { width: 300, height: 100 };
+    case "text":
+    default:
+      return { width: 200, height: 20 };
+  }
+};
+
+const LoadingSkeleton: React.FC<LoadingSkeletonProps> = ({
+  width,
+  height,
+  variant = "text",
+}) => {
+  const defaultSize = getDefaultSize(variant);
+  const style = {
+    width: width ?? defaultSize.width,
+    height: height ?? defaultSize.height,
+    borderRadius: getBorderRadius(variant),
+  } as React.CSSProperties;
+
+  return <div className={styles.skeleton} style={style} />;
+};
+
+export { LoadingSkeleton };
